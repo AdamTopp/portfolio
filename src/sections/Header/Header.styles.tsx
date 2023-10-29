25d7b8500dd2e15b002210769658b841c819b40e
AdamTopmay <adamtoppmayer@gmail.com>
2023-10-29 21:28:36 +0100

    [P-3] Add fonts and themeing

--- src/sections/Header/Header.styles.tsx
+++ src/sections/Header/Header.styles.tsx
@@ -1,62 +1,6 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    box-sizing: border-box;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    transform-style: preserve-3d;
-    z-index: -1;
-    background-color: #752121;
     height: 100vh;
-`;
-
-export const ContentWrapper = styled.div<{ paralaxSpeed: string }>`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    transform: translateY(${(props) => props.paralaxSpeed});
-    
-`;
-
-export const Title = styled.h1`
-    font-family: Pathway Gothic One;
-    white-space: nowrap;
-    color: black;
-    position: relative;
-    font-size: min(19rem, 19vw);
-    margin: 0;
-    text-align: center;
-
-    letter-spacing: -8px;
-    text-transform: uppercase;
-    &::before, &::after {
-        content: "Hi I'm Adam";
-        white-space: nowrap;
-        display: block;
-        position: absolute;
-        top:0;
-        color: rgba(0, 0, 0, 0);
-        -webkit-text-stroke: 1.5px #161616;
-        z-index: -1;
-    }
-
-    &::before {
-        transform: translateY(calc(min(14rem, 17vw) * -1));
-    }
-
-    &::after {
-        transform: translateY(min(14rem, 17vw));
-    }
-`;
-
-export const SubTitle = styled.h2`
-    color: white;
-    font-size: min(2rem, 4vw);
-    z-index: 1;
+    background-color: ${(props) => props.theme.colors.red};
 `;
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,34 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { ContentWrapper, SubTitle, Title, Wrapper } from './Header.styles'
+import React from 'react'
+import { Wrapper } from './Header.styles'
 
-function Header() {
-  const [speed, setSpeed] = useState('0px');
-
-  const handleScroll = () => {
-    const speed = `${window.scrollY * 0.4}px`;
-    setSpeed(speed)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [])
-
+const Header = () => {
   return (
     <Wrapper>
-      <ContentWrapper paralaxSpeed={speed}>
-        <SubTitle>
-          Do you need a website?
-        </SubTitle>
-        <Title>
-          Hi I'm Adam
-        </Title>
-        <SubTitle>
-          Let me help!
-        </SubTitle>
-      </ContentWrapper>
+
     </Wrapper>
   )
 }
