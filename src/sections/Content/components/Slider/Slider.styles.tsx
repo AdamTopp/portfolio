c74d50b652bf2117122811b269490df23e0aba28
AdamTopmay <adamtoppmayer@gmail.com>
2023-11-01 20:42:05 +0100

    [P-5] Slider css fixes

--- src/sections/Content/components/Slider/Slider.styles.tsx
+++ src/sections/Content/components/Slider/Slider.styles.tsx
@@ -11,41 +11,41 @@ const scroll = keyframes`
 export const Wrapper = styled.div`
     position: relative;
     align-self: flex-start;
-    max-width: 100vw;
     width: 100%;
+    max-width: 100vw;
     margin-bottom: 33rem;
 `;
 
 export const SliderWrapper = styled.div<{ imageSize: number }>`
     position: absolute;
     overflow: hidden;
-    top: ${(props) => props.imageSize / 2}px;
     transform: translateY(-50%);
+    top: ${(props) => props.imageSize / 2}px;
     z-index: 0;
 `;
 
 export const SliderItem = styled.li`
-    font-size: 120px;
+    white-space: nowrap;
+    font-size: 7rem;
     font-family: ${theme.fonts.family.PathwayGothic};
     -webkit-text-stroke: 1.5px ${theme.colors.red};
-    color: transparent;
     text-transform: uppercase;
-    white-space: nowrap;
+    color: transparent;
     cursor: default;
 `;
 
 export const SliderInner = styled.ul`
     display: flex;
     gap: 2rem;
+    width: fit-content;
     padding-block: 1rem;
     animation: ${scroll} 20s linear infinite;
-    width: fit-content;
 `;
 
 export const ImageWrapper = styled.div`
     position: relative;
-    display: flex;
     width: 100%;
+    display: flex;
     align-self: center;
     justify-content: center;
     z-index: 1;
@@ -55,19 +55,19 @@ export const ImageGrid = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-template-rows: auto auto;
-    padding: 0 8rem;
     grid-column-gap: 7vw;
-    max-width: ${theme.maxSize};
     width: 100%;
+    max-width: ${theme.maxSize};
+    padding: 0 8rem;
 `;
 
 export const ImageSection = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    aspect-ratio: 3/4;
     border-bottom: solid 2px ${theme.colors.white[20]};
     margin-bottom: 16px;
+    aspect-ratio: 3/4;
 `;
 
 export const Image = styled.div<{ active: boolean, angle: string }>`
@@ -76,7 +76,6 @@ export const Image = styled.div<{ active: boolean, angle: string }>`
     background-color: #da91917b;
     transition: all 400ms ease-out;
     transform: ${(props) => props.active ? 'scale(1)' : 'scale(0.9)'};
-    /* transition: rotate 500ms ease-out; */
     opacity: ${(props) => props.active ? 100 : 0};
     rotate: ${({active, angle}) => active ? angle : '0deg'};
 `;
@@ -89,20 +88,20 @@ export const Descritpion = styled.div`
 
 export const Title = styled.span<{ active?: boolean }>`
     width: fit-content;
-    transition: all 250ms;
+    margin-bottom: 1rem;
+    border-bottom: 2px solid ${({ active }) => active ? theme.colors.red : 'transparent'};
     font-size: min(2.6rem, 3vw);
     font-family: ${theme.fonts.family.Playfair};
-    border-bottom: 2px solid ${({ active }) => active ? theme.colors.red : 'transparent'};
     color: ${(props) => props.active ? theme.colors.white[100] : theme.colors.white[20]};
-    margin-bottom: 1rem;
+    transition: all 250ms;
     cursor: default;
 `;
 
 export const Text = styled.div<{ active?: boolean }>`
-    transition: color 250ms;
+    margin-bottom: 16px;
     font-size: 1.3rem;
     font-family: ${theme.fonts.family.Playfair};
     color: ${(props) => props.active ? theme.colors.white[80] : theme.colors.white[20]};
-    margin-bottom: 16px;
+    transition: color 250ms;
     cursor: default;
 `;
