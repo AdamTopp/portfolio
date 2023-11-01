d92c89e8c191acd2d538d03851fcdef2a64a4fda
AdamTopmay <adamtoppmayer@gmail.com>
2023-11-01 19:44:07 +0100

    [P-5] Slider images

--- src/sections/Content/components/Slider/Slider.styles.tsx
+++ src/sections/Content/components/Slider/Slider.styles.tsx
@@ -9,20 +9,28 @@ const scroll = keyframes`
 `
 
 export const Wrapper = styled.div`
+    position: relative;
     align-self: flex-start;
+    max-width: 100vw;
+    width: 100%;
 `;
 
-export const SliderWrapper = styled.div`
+export const SliderWrapper = styled.div<{ imageSize: number }>`
+    position: absolute;
     overflow: hidden;
+    top: ${(props) => props.imageSize / 2}px;
+    transform: translateY(-50%);
+    z-index: 0;
 `;
 
 export const SliderItem = styled.li`
-    font-size: 100px;
+    font-size: 120px;
     font-family: ${theme.fonts.family.PathwayGothic};
     -webkit-text-stroke: 1.5px ${theme.colors.red};
     color: transparent;
     text-transform: uppercase;
     white-space: nowrap;
+    cursor: default;
 `;
 
 export const SliderInner = styled.ul`
@@ -31,4 +39,66 @@ export const SliderInner = styled.ul`
     padding-block: 1rem;
     animation: ${scroll} 20s linear infinite;
     width: fit-content;
+`;
+
+export const ImageWrapper = styled.div`
+    position: relative;
+    display: flex;
+    width: 100%;
+    align-self: center;
+    justify-content: center;
+    z-index: 1;
+`;
+
+export const ImageGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto auto;
+    padding: 0 5rem;
+    grid-column-gap: 6vw;
+    max-width: ${theme.maxSize};
+    width: 100%;
+`;
+
+export const ImageSection = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    aspect-ratio: 3/4;
+    border-bottom: solid 2px ${theme.colors.white[20]};
+    margin-bottom: 16px;
+`;
+
+export const Image = styled.div<{ active: boolean }>`
+    width: 65%;
+    aspect-ratio: 3/4;
+    background-color: #da91917b;
+    transition: all 400ms ease-out;
+    transform: ${(props) => props.active ? 'scale(1)' : 'scale(0.9)'};
+    /* transition: rotate 500ms ease-out; */
+    opacity: ${(props) => props.active ? 100 : 0};
+    rotate: ${(props) => props.active ? '3deg' : '0deg'};
+`;
+
+export const Descritpion = styled.div`
+
+`;
+
+export const Title = styled.div<{ active?: boolean }>`
+    transition: color 250ms;
+    font-size: min(50px, 3vw);
+    font-family: ${theme.fonts.family.Playfair};
+    color: ${(props) => props.active ? theme.colors.white[100] : theme.colors.white[20]};
+    margin-bottom: 16px;
+    cursor: default;
+`;
+
+export const Text = styled.div<{ active?: boolean }>`
+    transition: color 250ms;
+    font-size: 20px;
+    font-family: ${theme.fonts.family.Playfair};
+    color: ${(props) => props.active ? theme.colors.white[60] : theme.colors.white[20]};
+    margin-bottom: 16px;
+    text-wrap: balance;
+    cursor: default;
 `;
--- src/sections/Content/components/Slider/Slider.tsx
+++ src/sections/Content/components/Slider/Slider.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { SliderInner, SliderItem, SliderWrapper, Wrapper } from './Slider.styles'
+import { Descritpion, Text, ImageSection, Image, ImageGrid, ImageWrapper, SliderInner, SliderItem, SliderWrapper, Title, Wrapper } from './Slider.styles'
 
 interface SliderItemGroupProps {
   n: number;
@@ -7,14 +7,18 @@ interface SliderItemGroupProps {
 
 const Slider = () => {
   const sliderItemRef = useRef<HTMLElement>();
+  const imageWrapperRef = useRef<HTMLElement>();
   const [sliderItemCount, setSliderItemCount] = useState(0);
+  const [imageSize, setImageSize] = useState(0);
+  const [hoverIndex, setHoverIndex] = useState(0);
 
   const handleResize = () => {
     const windowWidth = window.innerWidth;
     const itemWidth = sliderItemRef.current?.offsetWidth;
-    if (itemWidth) {
-      console.log(Math.ceil(windowWidth / itemWidth))
+    const itemHeight = imageWrapperRef.current?.offsetHeight;
+    if (itemWidth && itemHeight) {
       setSliderItemCount(Math.ceil(windowWidth / itemWidth));
+      setImageSize(itemHeight);
     } else {
       setSliderItemCount(0);
     }
@@ -36,13 +40,44 @@ const Slider = () => {
 
   return (
     <Wrapper>
-      <SliderWrapper>
+      <SliderWrapper imageSize={imageSize}>
         <SliderInner>
           <SliderItem ref={sliderItemRef}>What can I offer?</SliderItem>
           <SliderItemGroup n={sliderItemCount - 1}/>
           <SliderItemGroup n={sliderItemCount}/>
         </SliderInner>
       </SliderWrapper>
+      <ImageWrapper>
+        <ImageGrid>
+          <ImageSection ref={imageWrapperRef} onMouseEnter={() => setHoverIndex(0)}>
+            <Image active={hoverIndex === 0}/>
+          </ImageSection>
+          <ImageSection onMouseEnter={() => setHoverIndex(1)}>
+            <Image active={hoverIndex === 1}/>
+          </ImageSection>
+          <ImageSection onMouseEnter={() => setHoverIndex(2)}>
+            <Image active={hoverIndex === 2}/>
+          </ImageSection>
+          <Descritpion onMouseEnter={() => setHoverIndex(0)}>
+            <Title active={hoverIndex === 0}>Style</Title>
+            <Text active={hoverIndex === 0}>
+              I bring deep understanding of the React framework where combined with my knowledge of HTML, CSS and impeccable taste no design is out of my reach.
+            </Text>
+          </Descritpion>
+          <Descritpion onMouseEnter={() => setHoverIndex(1)}>
+            <Title active={hoverIndex === 1}>Code</Title>
+            <Text active={hoverIndex === 1}>
+              Throughout the years I gained experience not only in the Frontend side of web development but I had to learn technologies like Java, Node.js, Docker.
+            </Text>
+          </Descritpion>
+          <Descritpion onMouseEnter={() => setHoverIndex(2)}>
+            <Title active={hoverIndex === 2}>Communication</Title>
+            <Text active={hoverIndex === 2}>
+              Apart from my technical responsibilities I consider clarity in communication as important as code itself.
+            </Text>
+          </Descritpion>
+        </ImageGrid>
+      </ImageWrapper>
     </Wrapper>
   )
 }
